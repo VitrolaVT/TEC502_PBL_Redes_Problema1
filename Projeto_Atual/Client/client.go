c70b68111cbc7f6b05f9ffbe13cc14525e632377
VitrolaVT <victor.ariel25.vm@gmail.com>
2025-09-14 21:33:06 -0300

    Funções básics de pareamento, mensagem e abertura de pacotes - Ainda há erros

---
+++ Projeto_Atual/Client/client.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+// Struct como modelo de requisição do cliente para servidor
+type Requisicao struct {
+	Tipo            string `json:"tipo"`
+	Id_remetente    string `json:"id_remetente"`
+	Id_destinatario string `json:"id_destinatario"`
+	Mensagem        string `json:"mensagem"`
+	Carta           Tanque `json:"carta"`
+}
+
+// Struct modelo de resposta do servidor para cliente
+type Resposta struct {
+	Tipo     string   `json:"tipo"`
+	Mensagem string   `json:"mensagem"`
+	Cartas   []Tanque `json:"cartas"`
+}
+
+// Carta do jogo
+type Tanque struct {
+	Modelo     string `json:"modelo"`
+	Id_jogador string `json:"id_jogador"`
+	Vida       int    `json:"vida"`
+	Ataque     int    `json:"ataque"`
+}
+
+// Estados da máquina de estados
+const (
+	EstadoLivre = iota
+	EstadoPareado
+	EstadoEsperandoResposta
+	Estado_Batalhando
+)
+
+// Variável IP do server
+var ipServer string = "172.16.103.1"
+
+// Variáveis para informações pertinentes ao jogador
+var idPessoal, idParceiro string
+var minhasCartas []Tanque
+
+func main() {
+	//Conexão do tipo TCP com o servidor
+	conn, err := net.Dial("tcp", "10.0.0.117:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	//Estado atual do jogador
+	var estadoAtual int
+	estadoAtual = EstadoLivre
+
+	//Lista para guardar deck de batalha de uma possível batalha
+	deckBatalha := make([]Tanque, 0, 5)
+
+	idPessoal = "none"
+	idParceiro = "none"
+	//Goroutine (thread) para ouvir respostas do servidor
+	go func() {
+		for {
+			resposta := lerResposta(conn)
+			switch resposta.Tipo {
+			case "Erro":
+				color.Red("Erro: %s", resposta.Mensagem)
+				if idParceiro == "none" {
+					estadoAtual = EstadoLivre
+				} else {
+					estadoAtual = EstadoPareado
+				}
+
+			case "Criaçao_Id":
+				color.Yellow("Seu ID é %s", resposta.Mensagem)
+				idPessoal = resposta.Mensagem
+
+			case "Pareamento":
+				color.Green("Pareamento realizado com %s", resposta.Mensagem)
+				idParceiro = resposta.Mensagem
+				estadoAtual = EstadoPareado
+
+			case "Mensagem":
+				color.Cyan("Mensagem recebida: %s", resposta.Mensagem)
+
+			case "Sorteio":
+				minhasCartas = append(minhasCartas, resposta.Cartas...)
+
+				color.Green(resposta.Tipo, resposta.Mensagem)
+				imprimirTanques(resposta.Cartas)
+
+			case "Inicio_Batalha":
+				color.Yellow("Batalha iniciada com %s", resposta.Mensagem)
+				deckBatalha = nil
+				deckBatalha = append(deckBatalha, sortearDeck()...)
+				color.Cyan("Seu deck de batalha é:")
+				imprimirTanques(deckBatalha)
+				estadoAtual = Estado_Batalhando
+
+			case "Fim_Batalha":
+				color.Yellow("Batalha finalizada!")
+				color.Cyan("O vencedor foi o jogador %s!", resposta.Mensagem)
+				estadoAtual = EstadoPareado
+
+			case "Enviar_Próxima_Carta":
+				indice, err := strconv.Atoi(resposta.Mensagem)
+
+				if err != nil {
+					fmt.Println("Erro ao converter:", err)
+					panic(err)
+				}
+
+				enviarRequisicao(conn, Requisicao{
+					Tipo: "Próxima_Carta", Id_remetente: idPessoal, Id_destinatario: idParceiro,
+					Mensagem: "Carta", Carta: deckBatalha[indice]})
+
+			case "Turno_Realizado":
+				color.Yellow("Turno Realizado!")
+
+			default:
+				fmt.Println("Resposta recebida com tipo desconhecido: ", resposta.Tipo)
+			}
+		}
+	}()
+
+	//Loop infinito e centralizado que lê do terminal
+	reader := bufio.NewReader(os.Stdin)
+	for {
+
+		switch estadoAtual {
+		case EstadoLivre:
+			fmt.Println("Comando Parear <id> / Abrir / sair: ")
+			line, _ := reader.ReadString('\n')
+			line = strings.TrimSpace(line)
+
+			if line == "sair" {
+				os.Exit(0)
+			}
+
+			if strings.HasPrefix(line, "Parear ") {
+				idDestinatario := strings.TrimPrefix(line, "Parear ")
+				enviarRequisicao(conn, Requisicao{Tipo: "Parear", Id_remetente: idPessoal, Id_destinatario: idDestinatario, Mensagem: "None"})
+				estadoAtual = EstadoEsperandoResposta
+			} else if strings.HasPrefix(line, "Abrir ") {
+				enviarRequisicao(conn, Requisicao{Tipo: "Abrir_Pacote", Id_remetente: idPessoal, Id_destinatario: "None", Mensagem: "None"})
+			} else {
+				color.Red("Comando inválido")
+			}
+
+		case EstadoPareado:
+			fmt.Println("Comando Abrir / Mensagem / Batalhar / sair: ")
+			line, _ := reader.ReadString('\n')
+			line = strings.TrimSpace(line)
+
+			if line == "sair" {
+				os.Exit(0)
+			}
+
+			if strings.HasPrefix(line, "Abrir ") {
+				enviarRequisicao(conn, Requisicao{Tipo: "Abrir_Pacote", Id_remetente: idPessoal, Id_destinatario: "None", Mensagem: "None"})
+			} else if strings.HasPrefix(line, "Batalhar ") {
+				if len(minhasCartas) < 5 {
+					color.Red("Você não tem cartas suficientes para montar um deck")
+				} else {
+					enviarRequisicao(conn, Requisicao{Tipo: "Batalhar", Id_remetente: idPessoal, Id_destinatario: idParceiro, Mensagem: "None"})
+					estadoAtual = EstadoEsperandoResposta
+				}
+			} else if strings.HasPrefix(line, "Mensagem ") {
+				mensagem := strings.TrimPrefix(line, "Mensagem ")
+				enviarRequisicao(conn, Requisicao{Tipo: "Mensagem", Id_remetente: idPessoal, Id_destinatario: idParceiro, Mensagem: mensagem})
+
+			} else {
+				color.Red("Comando inválido")
+			}
+
+		case EstadoEsperandoResposta:
+			color.Yellow("Esperando resposta do server")
+			time.Sleep(1 * time.Second)
+
+		case Estado_Batalhando:
+			color.Yellow("Batalha ocorrendo!!")
+			time.Sleep(5 * time.Second)
+
+		default:
+			color.Red("Estado indefinido")
+		}
+	}
+}
+
+// Função para enviar requisição através de um pacote formato json via conexão TCP
+func enviarRequisicao(conn net.Conn, requisicao Requisicao) {
+	requisicao_json, _ := json.Marshal(requisicao)
+	conn.Write(append(requisicao_json, '\n'))
+}
+
+// Função para ler da conexão uma resposta do servidor e transformar de volta em struct
+func lerResposta(conn net.Conn) Resposta {
+	reader := bufio.NewReader(conn)
+	mensagem, _ := reader.ReadBytes('\n')
+	var resposta Resposta
+	json.Unmarshal(mensagem, &resposta)
+	return resposta
+}
+
+// Função para sortear 5 cartas da coleção de cartas do jogador
+func sortearDeck() []Tanque {
+	//Cria um gerador aleatório independente usando tempo da chamada da função
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	//Sorteia 5 índices usando o gerador independente
+	n := len(minhasCartas)
+	indices := r.Perm(n)[:5]
+
+	deck := make([]Tanque, 0, 5)
+	for _, i := range indices {
+		deck = append(deck, minhasCartas[i])
+	}
+
+	return deck
+}
+
+// Função para imprimir a lista de tanques/cartas
+func imprimirTanques(lista []Tanque) {
+	for i, t := range lista {
+		fmt.Printf("Tanque %d:\n", i+1)
+		fmt.Printf("  Modelo: %s\n", t.Modelo)
+		color.Yellow("  Jogador: %s", t.Id_jogador)
+		color.Green("  Vida: %d", t.Vida)
+		color.Red("  Ataque: %d", t.Ataque)
+	}
+}
---
+++ Projeto_Atual/Server/server.go
@@ -0,0 +1,252 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+// Struct como modelo de requisição do cliente para servidor
+type Requisicao struct {
+	Tipo            string `json:"tipo"`
+	Id_remetente    string `json:"id_remetente"`
+	Id_destinatario string `json:"id_destinatario"`
+	Mensagem        string `json:"mensagem"`
+	Carta           Tanque `json:"carta"`
+}
+
+// Struct modelo de resposta do servidor para cliente
+type Resposta struct {
+	Tipo     string   `json:"tipo"`
+	Mensagem string   `json:"mensagem"`
+	Cartas   []Tanque `json:"cartas"`
+}
+
+// Carta do jogo
+type Tanque struct {
+	Modelo     string `json:"modelo"`
+	Id_jogador string `json:"id_jogador"`
+	Vida       int    `json:"vida"`
+	Ataque     int    `json:"ataque"`
+}
+
+// Variáveis do server
+var (
+	clientes      = make(map[string]net.Conn) //Map para guardar conexões através dos IDs
+	muClientes    sync.RWMutex                //Mutex para sincronização dos jogadores
+	pares         = make(map[string]string)   //Pares de jogadores conectados
+	muPares       sync.RWMutex                //Mutex para sincronização de jogadores pareados
+	idCounter     int                         //Contador do ID
+	pacoteCounter = 10                        //Contador de pacotes disponíveis
+	muPacote      sync.Mutex                  //Mutex para sincronização do estoques
+)
+
+// Pacote 1 de cartas
+var pacote_1 = []Tanque{
+	//5 leves
+	{"Coyote (Light)", "server", 50, 10},
+	{"Falcon (Light)", "server", 55, 12},
+	{"Wolf (Light)", "server", 60, 15},
+	{"Fox (Light)", "server", 52, 11},
+	{"Hawk (Light)", "server", 58, 14},
+
+	//3 médios
+	{"Rhino (Medium)", "server", 100, 25},
+	{"Jaguar (Medium)", "server", 110, 27},
+	{"Panther (Medium)", "server", 120, 30},
+
+	//2 pesados
+	{"Mammoth (Heavy)", "server", 200, 50},
+	{"Titan (Heavy)", "server", 220, 55},
+}
+
+func main() {
+	//Criação de porta
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		color.Red("Erro na criação da porta")
+		panic(err)
+	}
+	color.Green("Servidor rodando na porta 8080")
+
+	//Ouvir constantemente requisições de conexão
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			color.Red("Erro na aceitação da conexão")
+			continue
+		}
+		//Goroutine para cada conexão/jogador
+		go criarConexao(conn)
+	}
+}
+
+// Função para criar a conexão e garantir transferência de informações cliente/servidor
+func criarConexao(conn net.Conn) {
+	defer conn.Close()
+
+	//Criar id e guardar no map
+	muClientes.Lock()
+	idCounter++
+	id_cliente := fmt.Sprintf("%d", idCounter)
+	clientes[id_cliente] = conn
+	muClientes.Unlock()
+	color.Cyan("Jogador conectado! ID = %s", id_cliente)
+
+	resposta := Resposta{Tipo: "Criaçao_Id", Mensagem: id_cliente}
+	enviarResposta(conn, resposta)
+
+	//Ler constantemente coisas enviados pelo outro lado da conexão
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadBytes('\n')
+		if err != nil {
+			muClientes.Lock()
+			delete(clientes, id_cliente)
+			muClientes.Unlock()
+
+			delete(pares, id_cliente)
+			return
+		}
+
+		var requisicao Requisicao
+		err = json.Unmarshal(msg, &requisicao) //Deserializar requisição do json
+		if err != nil {
+			resposta.Tipo = "Erro"
+			resposta.Mensagem = "Erro no recebimento do json"
+			enviarResposta(conn, resposta)
+		}
+
+		//Decodificar o tipo da requisição
+		switch requisicao.Tipo {
+		case "Parear":
+			parearClientes(conn, requisicao.Id_remetente, requisicao.Id_destinatario)
+
+		case "Mensagem":
+			transmitirMensagem(conn, requisicao.Id_remetente, requisicao.Id_destinatario, requisicao.Mensagem)
+
+		case "Abrir_Pacote":
+			sortearCartas(conn)
+
+		default:
+			resposta.Tipo = "Erro"
+			resposta.Mensagem = "Comando inválido"
+			enviarResposta(conn, resposta)
+		}
+	}
+}
+
+// Função para enviar resposta serializada em formato json para o cliente
+func enviarResposta(conn net.Conn, resposta Resposta) {
+	resposta_json, _ := json.Marshal(resposta)
+	conn.Write(append(resposta_json, '\n'))
+}
+
+// Função para parear 2 jodadores
+func parearClientes(conn net.Conn, id_remetente, id_destinatario string) {
+	var resposta Resposta
+
+	if id_remetente == id_destinatario {
+		resposta.Tipo = "Erro"
+		resposta.Mensagem = "Id destinatário não pode ser igual ao Id remetente"
+		enviarResposta(conn, resposta)
+		return
+	} else if _, existe := clientes[id_destinatario]; !existe {
+		resposta.Tipo = "Erro"
+		resposta.Mensagem = "Id destinatário não existe"
+		enviarResposta(conn, resposta)
+		return
+	} else if _, existe := pares[id_remetente]; existe {
+		resposta.Tipo = "Erro"
+		resposta.Mensagem = "Já existe um pareamento existente para o remetente"
+		enviarResposta(conn, resposta)
+		return
+	} else if _, existe := pares[id_destinatario]; existe {
+		resposta.Tipo = "Erro"
+		resposta.Mensagem = "Já existe um pareamento existente para o destinatário"
+		enviarResposta(conn, resposta)
+		return
+	}
+
+	//Bloquear acesso da variáveis de pares e jogadores para sincronização
+	muPares.Lock()
+	pares[id_remetente] = id_destinatario
+	pares[id_destinatario] = id_remetente
+	muPares.Unlock()
+
+	resposta.Tipo = "Pareamento"
+	resposta.Mensagem = id_destinatario
+	enviarResposta(conn, resposta)
+
+	//Garantir leitura sincronizada entre goroutines que também lêem a variável
+	muClientes.RLock()
+	resposta.Mensagem = id_remetente
+	enviarResposta(clientes[id_destinatario], resposta)
+	muClientes.RUnlock()
+}
+
+// Função para mandar mensagem de um jogador para o outro pareado
+func transmitirMensagem(conn net.Conn, id_remetente, idDestinatario, mensagem string) {
+	//Garantir leitura sincronizada entre goroutines que também lêem a variável
+	muPares.RLock()
+	idPar, existe := pares[id_remetente]
+	muPares.RUnlock()
+
+	var resposta Resposta
+	if idPar != idDestinatario || !existe {
+		resposta.Tipo = "Erro"
+		resposta.Mensagem = "Id do destinatário difente da conexão existente ou não existe conexão"
+		enviarResposta(conn, resposta)
+		return
+	}
+
+	//Garantir leitura sincronizada entre goroutines que também lêem a variável
+	muClientes.RLock()
+	idParConn := clientes[idPar]
+	muClientes.RUnlock()
+
+	resposta.Tipo = "Mensagem"
+	resposta.Mensagem = mensagem
+	enviarResposta(idParConn, resposta)
+}
+
+// Função de sortear cartas do pacote escolhido
+func sortearCartas(conn net.Conn) {
+	//Cria um gerador aleatório independente usando tempo da chamada da função
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	//Bloquear acesso ao contador de pacotes disponíveis
+	muPacote.Lock()
+	defer muPacote.Unlock()
+
+	var resposta Resposta
+	if pacoteCounter <= 0 {
+		resposta.Tipo = "Erro"
+		resposta.Mensagem = "Não há mais pacotes disponíveis"
+		enviarResposta(conn, resposta)
+		return
+	}
+
+	pacoteCounter--
+
+	//Sorteia 5 índices usando o gerador independente
+	n := len(pacote_1)
+	indices := r.Perm(n)[:5]
+
+	cartasSorteadas := make([]Tanque, 0, 5)
+
+	for _, i := range indices {
+		cartasSorteadas = append(cartasSorteadas, pacote_1[i])
+	}
+
+	resposta.Tipo = "Sorteio"
+	resposta.Mensagem = "Sorteio realizado com sucesso"
+	resposta.Cartas = cartasSorteadas
+
+	enviarResposta(conn, resposta)
+}
